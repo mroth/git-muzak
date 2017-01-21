3c46342233395f8f65ba4ac634b8a764fe31bb3d
Matthew Rothenberg <mrothenberg@gmail.com>
2017-01-21 13:30:39 -0500

    avoid error if Spotify not installed
    sending a tell to a nonexistent application bombs out in AppleScript,
    so need to make sure application exists at all before checking to see
    if it’s running.
    
    iTunes should always be there since we’re assuming a macOS system for
    now, but might as well test for it too for future-proofing, even if
    verbose in current methodology.
    
    ♬ : The Clash / Lost In the Supermarket

--- git-muzak.sh
+++ git-muzak.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+debug_msg() { [[ $DEBUG = "true" ]] && >&2 echo "DEBUG: $1" ; }
 
 show_help() {
 cat << EOF
@@ -45,18 +46,36 @@ install_hook() {
 ### music related functions ###
 ###############################
 
+# current itunes track, if active and playing
 itunes_track() {
     osascript -e 'tell application "iTunes" to if it is running and player state is playing then "♬ : " & artist of current track & " / " & name of current track'
 }
 
+# current spotify track, if active and playing
 spotify_track() {
     osascript -e 'tell application "Spotify" to if it is running and player state is playing then "♬ : " & artist of current track & " / " & name of current track'
 }
 
+# returns bundle identifier string if exists, blank if not
+app_exists() {
+    osascript -e 'try' -e "id of application \"$1\"" -e 'end try'
+}
+
+
 current_track() {
-    ITUNES=$(itunes_track)
-    SPOTIFY=$(spotify_track)
-    echo "${ITUNES:-$SPOTIFY}"
+    if [[ $(app_exists "iTunes") ]]; then
+        active_track=$(itunes_track)
+        if [[ $active_track ]]; then
+            echo "$active_track"; return
+        fi
+    fi
+
+    if [[ $(app_exists "Spotify") ]]; then
+        active_track=$(spotify_track)
+        if [[ $active_track ]]; then
+            echo "$active_track"; return
+        fi
+    fi
 }
 
 
